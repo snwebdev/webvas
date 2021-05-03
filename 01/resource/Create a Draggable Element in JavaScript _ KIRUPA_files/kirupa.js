249b0b5d187100c79a4054bce82bfe58e8602fe8
stewart <stewartnairn@live.com>
2021-05-03 22:23:42 +0100

    first commit

---
+++ 01/resource/Create a Draggable Element in JavaScript _ KIRUPA_files/kirupa.js
@@ -0,0 +1,2 @@
+function hideDisqus(){var dataHelp=document.querySelector("body").getAttribute("data-help");var oldComments=document.querySelector("#oldComments");if(dataHelp==null){if(oldComments){oldComments.style.display="none";}}}
+document.documentElement.setAttribute("lang","en");
